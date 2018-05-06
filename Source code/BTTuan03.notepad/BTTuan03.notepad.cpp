00fd3c44ebe29bad6a4549e910dce2d0d86c2d50
duonghieu121097 <duonghieu121097@gmail.com>
2018-05-06 17:32:08 +0700

    Upload

---
+++ Source code/BTTuan03.notepad/BTTuan03.notepad.cpp
@@ -0,0 +1,241 @@
+// BBTuan03.Notepad.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "BTTuan03.notepad.h"
+#include <windowsx.h>
+#include <string>
+#include <fstream>
+#define MAX_LOADSTRING 100
+
+using namespace std;
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify);
+void OnPaint(HWND hwnd);
+void OnDestroy(HWND hwnd);
+BOOL OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct);
+
+void OnOpenFile();
+void OnSaveFile();
+
+HWND hwndEdit = NULL;
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	// TODO: Place code here.
+
+	// Initialize global strings
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDC_BTTUAN03NOTEPAD, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// Perform application initialization:
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_BTTUAN03NOTEPAD));
+
+	MSG msg;
+
+	// Main message loop:
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+	return (int)msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEXW wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDC_BTTUAN03NOTEPAD));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_BTTUAN03NOTEPAD);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassExW(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+	hInst = hInstance; // Store instance handle in our global variable
+
+	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+	if (!hWnd)
+	{
+		return FALSE;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE:  Processes messages for the main window.
+//
+//  WM_COMMAND  - process the application menu
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+		HANDLE_MSG(hWnd, WM_PAINT, OnPaint);
+		HANDLE_MSG(hWnd, WM_CREATE, OnCreate);
+		HANDLE_MSG(hWnd, WM_COMMAND, OnCommand);
+		HANDLE_MSG(hWnd, WM_DESTROY, OnDestroy);
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
+void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
+{
+	switch (id)
+	{
+	case IDM_ABOUT:
+		DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hwnd, About);
+		break;
+	case IDM_EXIT:
+		DestroyWindow(hwnd);
+		break;
+	case ID_FILE_OPEN:
+		OnOpenFile();
+		break;
+	case ID_FILE_SAVE:
+		OnSaveFile();
+		break;
+	case ID_EDIT_COPY:
+		SendMessage(hwndEdit, WM_COPY, 0, 0);
+		break;
+	case ID_EDIT_CUT:
+		SendMessage(hwndEdit, WM_CUT, 0, 0);
+		break;
+	case ID_EDIT_PASTE:
+		SendMessage(hwndEdit, WM_PASTE, 0, 0);
+		break;
+	}
+}
+void OnPaint(HWND hwnd)
+{
+	PAINTSTRUCT ps;
+	HDC hdc = BeginPaint(hwnd, &ps);
+	// TODO: Add any drawing code that uses hdc here...
+	EndPaint(hwnd, &ps);
+}
+void OnDestroy(HWND hwnd)
+{
+	PostQuitMessage(0);
+}
+BOOL OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct)
+{
+	RECT rcClient;
+	GetWindowRect(hwnd, &rcClient);
+	int width = rcClient.right - rcClient.left;
+	int height = rcClient.bottom - rcClient.top;
+	hwndEdit = CreateWindow(L"EDIT", L"", WS_VISIBLE | WS_CHILD | ES_AUTOHSCROLL | WS_BORDER
+		, 0, 0, width, height, hwnd, NULL, hInst, NULL);
+	return true;
+}
+
+
+void OnOpenFile()
+{
+	wfstream f;
+	f.open("gioithieu.txt", ios::in);
+	f.seekg(0, ios::end);
+	int length = f.tellg();
+	WCHAR* buffer = new WCHAR[length];
+	f.seekg(0, ios::beg);
+	f.read(buffer, length);
+	SetWindowText(hwndEdit, buffer);
+	delete buffer;
+	f.close();
+}
+void OnSaveFile()
+{
+	wfstream f;
+	f.open("newfile.txt", ios::out);
+	int size = GetWindowTextLength(hwndEdit);
+	WCHAR* buffer = new WCHAR[size + 1];
+	GetWindowText(hwndEdit, buffer, size + 1);
+	f << buffer;
+	delete buffer;
+	f.close();
+}
